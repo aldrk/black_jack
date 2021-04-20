95a777d4a5add750742af916fedcedc71b6061d2
Alexandr <sashka.krup@gmail.com>
2021-04-20 11:51:58 +0300

    add CardDeck class

---
+++ deck.rb
@@ -0,0 +1,22 @@
+class CardDeck
+  CARD_SUITS = %w[♡ ♧ ♢ ♤].freeze
+  CARD_VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+  attr_reader :cards
+
+  def initialize
+    @cards = create_deck
+  end
+
+  private
+
+  def create_deck
+    cards = []
+    CARD_SUITS.each do |suit|
+      CARD_VALUES.each do |value|
+        cards << Card.new(suit, value)
+      end
+    end
+    cards.shuffle
+  end
+end
